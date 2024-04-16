38d764511a484b1c3170eb4c0fbe875797208920
Nikola <nikola@playform.cloud>
2024-04-16 04:40:09 +0300

    squash!

--- Target/Class/Adapt.js
+++ Target/Class/Adapt.js
@@ -1,2 +1,2 @@
 #!/usr/bin/env node
-var e=new(await import("commander")).Command().name("Adapt").version("0.0.1").description("\u{1F50C}\u2000Adapt").argument("<File...>","File.").action((await import("../Function/Adapt.js")).default).parse();export{e as default};
+var e=new(await import("commander")).Command().name("Adapt").version("0.0.1").description("\u{1F50C}\u2000Adapt.").argument("<File...>","File.").action((await import("../Function/Adapt.js")).default).parse();export{e as default};
