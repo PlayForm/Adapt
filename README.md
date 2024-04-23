78e26bef287e4f1a20e885a5719e7a852e379876
Nikola R. Hristov <nikola@playform.cloud>
2024-04-23 10:37:51 +0300

    squash!

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 🔌 [Adapt.]
+# 🔌 [Adapt.]
 
 `Adapt` modifies your `package.json` on preinstall and allows you to choose a
 strategy when using a `peerDependency` in an upstream or downstream packages to
--- Source/Class/Adapt.ts
+++ Source/Class/Adapt.ts
@@ -7,7 +7,7 @@
 export default new (await import("commander")).Command()
 	.name("Adapt")
 	.version(process.env["VERSION_PACKAGE"] ?? "0.0.1")
-	.description("🔌 Adapt.")
+	.description("🔌 Adapt.")
 	.argument("<File...>", "File.")
 	.action((await import("@Function/Adapt.js")).default)
 	.parse();
