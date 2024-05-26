34f4830627efe92f6b678af800fd7d1c934b76bc
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-26 17:15:44 +0300

    squash!

--- Source/Function/Adapt.ts
+++ Source/Function/Adapt.ts
@@ -15,5 +15,5 @@ export const { resolve } = await import("path");
 export const Pipe: string[] = [];
 
 export const Current = (await import("url")).fileURLToPath(
-	(await import("path")).dirname(import.meta.url)
+	(await import("path")).dirname(import.meta.url),
 );
